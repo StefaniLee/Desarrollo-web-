230a3d0c77aa0ba01dc5991326351ac0dd27a94c
Stefani Lee <42162725+StefaniLee@users.noreply.github.com>
2022-03-22 11:51:38 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+function mostrar(id)
+        {
+            
+            if(document.getElementById(id).style.display=="block")
+            document.getElementById(id).style.display="none";
+            else
+            document.getElementById(id).style.display="block";
+        }
